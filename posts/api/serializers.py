d14eefdc288b0fbe04382433456af4420c386007
Augusto Rocha <augusto.rochaa08@gmail.com>
2025-06-12 11:03:04 -0300

    feat: add image field

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -5,6 +5,6 @@ from ..models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'author', 'title', 'content', 'image', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
         
